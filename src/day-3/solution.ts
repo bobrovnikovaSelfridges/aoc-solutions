ebae0fd14f0059f96b3a2a752ddc037d6d910596
Larisa <larisa.bobrovnikova@selfridges.co.uk>
2024-12-04 13:41:49 +0000

    added task 3 solution

--- src/day-3/solution.ts
+++ src/day-3/solution.ts
@@ -1,12 +1,9 @@
-import { printFile } from "../dev/print-file";
 import { inputDay3 as input } from "./input";
 
 // instructions like mul(X,Y), where X and Y are each 1-3 digit numbers.:
 const validInstructionRegex = /mul\((\d{1,3}),\s*(\d{1,3})\)/g;
 
-// from task example:
-const exampleFromTaskP1 = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
-
+// p.1.
 const getValidInstructions = (inputValue: string) => {
     const response = [...inputValue.matchAll(validInstructionRegex)]
     return response?.reduce((sum, matchedInstruction) => {
@@ -15,12 +12,7 @@ const getValidInstructions = (inputValue: string) => {
     }, 0)
 }
 
-// console.log(getValidInstructions(exampleFromTaskP1)) //161
-// console.log(getValidInstructions(input)) // 175700056
-
 // p.2.
-// const disabledInstructionsRegex = /don't\(\).*?do\(\)/gs;
-
 const getValidInstructionsWithUpdatedRules = (inputValue: string) => {
      const cleanedRecords = inputValue.split(`don't()`).map((el, index) => {
         if (index === 0) return el
@@ -39,3 +31,10 @@ getValidInstructionsWithUpdatedRules(input)
 // 83 524 390 too hgh too....
 //  20 719 720 is too low
 //  71 668 682 correct result
+
+// const disabledInstructionsRegex = /don't\(\).*?do\(\)/gs;
+// from task example:
+// const exampleFromTaskP1 = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+// console.log(getValidInstructions(exampleFromTaskP1)) //161
+// console.log(getValidInstructions(input)) // 175700056
+
